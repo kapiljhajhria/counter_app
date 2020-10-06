7715fb571bc0437177cab22a52019f06edb783ec
kapiljhajhria <jhajhria44@gmail.com>
2020-10-06 22:32:04 +0530

    using inline styles

--- src/components/counterComponent.jsx
+++ src/components/counterComponent.jsx
@@ -14,7 +14,7 @@ class Counter extends Component {
         return (
             <React.Fragment>
                 {/*<img src={this.state.imageUrl} alt="Random Image"/>*/}
-                <span style={this.styles} className="badge badge-primary m-2">{this.formatCount()}</span>
+                <span style={{fontSize:30}} className="badge badge-primary m-2">{this.formatCount()}</span>
                 <button className="btn btn-secondary btn-sm"> Increment</button>
             </React.Fragment>
         );
