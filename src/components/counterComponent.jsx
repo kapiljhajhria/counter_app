6a924880f40f08b6a9e41dd24d4e9ff2fd509eed
kapiljhajhria <jhajhria44@gmail.com>
2020-10-06 22:18:59 +0530

    using React.Fragment instead of using div

--- src/components/counterComponent.jsx
+++ src/components/counterComponent.jsx
@@ -1,10 +1,16 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 
 class Counter extends Component {
-  state = {};
-  render() {
-    return <h1>Hello World</h1>;
-  }
+    state = {};
+
+    render() {
+        return (
+            <React.Fragment>
+                <h1>Hello World</h1>
+                <button> Increment</button>
+            </React.Fragment>
+        );
+    }
 }
 
 export default Counter;
