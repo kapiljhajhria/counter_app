353601a4cc30e1e4980b047b50b03ae68a81582f
kapiljhajhria <jhajhria44@gmail.com>
2020-10-06 22:45:39 +0530

    rendering lists of items

--- src/components/counterComponent.jsx
+++ src/components/counterComponent.jsx
@@ -4,6 +4,7 @@ class Counter extends Component {
     state = {
         count: 0,
         // imageUrl:'https://picsum.photos/200'
+        tags: ['tag1', 'tag2', 'tag3']
     };
     styles = {
         fontSize: 50,
@@ -16,6 +17,9 @@ class Counter extends Component {
                 {/*<img src={this.state.imageUrl} alt="Random Image"/>*/}
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
                 <button className="btn btn-secondary btn-sm"> Increment</button>
+                <ul>
+                    {this.state.tags.map(tag => <li>{tag}</li>)}
+                </ul>
             </React.Fragment>
         );
     }
