b672a611d990fa6f526dff31d9e2d185bfbeb1a8
kapiljhajhria <jhajhria44@gmail.com>
2020-10-06 22:31:10 +0530

    using styles constant to apply styles

--- src/components/counterComponent.jsx
+++ src/components/counterComponent.jsx
@@ -5,12 +5,16 @@ class Counter extends Component {
         count: 0,
         // imageUrl:'https://picsum.photos/200'
     };
+    styles = {
+        fontSize: 50,
+        fontWeight:'bold'
+    }
 
     render() {
         return (
             <React.Fragment>
                 {/*<img src={this.state.imageUrl} alt="Random Image"/>*/}
-                <span className="badge badge-primary m-2">{this.formatCount()}</span>
+                <span style={this.styles} className="badge badge-primary m-2">{this.formatCount()}</span>
                 <button className="btn btn-secondary btn-sm"> Increment</button>
             </React.Fragment>
         );
