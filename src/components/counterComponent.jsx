16a0f70c14f8136ac4f910601c0a92e82f9f7b14
kapiljhajhria <jhajhria44@gmail.com>
2020-10-06 22:30:22 +0530

    using bootstrap classes to style counter, add marging

--- src/components/counterComponent.jsx
+++ src/components/counterComponent.jsx
@@ -3,15 +3,15 @@ import React, {Component} from "react";
 class Counter extends Component {
     state = {
         count: 0,
-        imageUrl:'https://picsum.photos/200'
+        // imageUrl:'https://picsum.photos/200'
     };
 
     render() {
         return (
             <React.Fragment>
-                <img src={this.state.imageUrl} alt="Random Image"/>
-                <span>{this.formatCount()}</span>
-                <button> Increment</button>
+                {/*<img src={this.state.imageUrl} alt="Random Image"/>*/}
+                <span className="badge badge-primary m-2">{this.formatCount()}</span>
+                <button className="btn btn-secondary btn-sm"> Increment</button>
             </React.Fragment>
         );
     }
