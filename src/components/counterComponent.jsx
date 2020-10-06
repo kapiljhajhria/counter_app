a4954f91d4ce6c2b1c3ccac2d400aa94823a8d7f
kapiljhajhria <jhajhria44@gmail.com>
2020-10-06 22:41:24 +0530

    render classes dynamically

--- src/components/counterComponent.jsx
+++ src/components/counterComponent.jsx
@@ -7,14 +7,16 @@ class Counter extends Component {
     };
     styles = {
         fontSize: 50,
-        fontWeight:'bold'
+        fontWeight: 'bold'
     }
 
     render() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? "warning" : "primary"
         return (
             <React.Fragment>
                 {/*<img src={this.state.imageUrl} alt="Random Image"/>*/}
-                <span style={{fontSize:30}} className="badge badge-primary m-2">{this.formatCount()}</span>
+                <span className={classes}>{this.formatCount()}</span>
                 <button className="btn btn-secondary btn-sm"> Increment</button>
             </React.Fragment>
         );
