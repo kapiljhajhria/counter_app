caa292bf374cdb64fd422ee9ddc4208f9ec4705e
kapiljhajhria <jhajhria44@gmail.com>
2020-10-07 21:12:17 +0530

    testing with functional component

--- src/components/counterComponent.jsx
+++ src/components/counterComponent.jsx
@@ -1,109 +1,107 @@
 import React from "react";
 
-// class Counter {
-//     constructor(props) {
-//         super(props);
-//         this.props = props;
-//         this.state = {
-//             value: this.props.,
-//             // imageUrl:'https://picsum.photos/200'
-//         }
-//     }
-//     ;
-//     // constructor() {
-//     //     super();
-//     //     this.handleIncrement = this.handleIncrement.bind(this);
-//     // }
-//
-//     // styles = {
-//     //     fontSize: 50,
-//     //     fontWeight: 'bold'
-//     // }
-//
-//     // renderTags() {
-//     //     if (this.state.tags.length === 0) return <p>No tags Found</p>
-//     //     return <ul>
-//     //         {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
-//     //     </ul>;
-//     // }
-//
-//     handleIncrement = (product) => {
-//         console.log("Increment Clicked");
-//         // this.state.count++;
-//         this.setState({
-//             value: this.state.value + 1
-//         });
-//         //obj.method() this will return ref to the object
-//         //function() this will return window object, if strict mode is enabled it will return undefined
-//         //to fix this issue use bind method
-//     }
-//     props;
-//
-//     render() {
-//         console.log(this)
-//         return (
-//             <React.Fragment>
-//                 {/*<img src={this.state.imageUrl} alt="Random Image"/>*/}
-//                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-//                 <button
-//                     onClick={() => {
-//                         this.handleIncrement("some product/event")
-//                     }}
-//                     className="btn btn-secondary btn-sm">
-//                     Increment
-//                 </button>
-//             </React.Fragment>
-//         );
-//     }
-//
-//     getBadgeClasses() {
-//         let classes = "badge m-2 badge-";
-//         classes += (this.state.value === 0) ? "warning" : "primary"
-//         return classes;
-//     }
-//
-//     formatCount() {
-//         const value = this.state.value;
-//         return value === 0 ? "Zero" : value;
-//     }
-// }
-//
-// export default Counter;
+class Counter extends  React.Component{
+    state = {
+        value: this.props.counterValue, ///TODO: fix props issue here
+        // imageUrl:'https://picsum.photos/200'
+    }
+    constructor(props) {
+        super(props);
 
-export default function Counter(props) {
-    ///TODO: fix props issue here
-    const [value, setValue] = React.useState(props.counterValue);
+    }
+    // constructor() {
+    //     super();
+    //     this.handleIncrement = this.handleIncrement.bind(this);
+    // }
 
+    // styles = {
+    //     fontSize: 50,
+    //     fontWeight: 'bold'
+    // }
 
-    function handleIncrement(product) {
+    // renderTags() {
+    //     if (this.state.tags.length === 0) return <p>No tags Found</p>
+    //     return <ul>
+    //         {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
+    //     </ul>;
+    // }
+
+    handleIncrement = (product) => {
         console.log("Increment Clicked");
-        let countValue = value;
-        console.log(props);
-        setValue(++countValue)
+        // this.state.count++;
+        this.setState({
+            value: this.state.value + 1
+        });
+        //obj.method() this will return ref to the object
+        //function() this will return window object, if strict mode is enabled it will return undefined
+        //to fix this issue use bind method
     }
 
-    function getBadgeClasses() {
+    render() {
+        console.log(this)
+        return (
+            <React.Fragment>
+                {/*<img src={this.state.imageUrl} alt="Random Image"/>*/}
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button
+                    onClick={() => {
+                        this.handleIncrement("some product/event")
+                    }}
+                    className="btn btn-secondary btn-sm">
+                    Increment
+                </button>
+            </React.Fragment>
+        );
+    }
+
+    getBadgeClasses=()=> {
         let classes = "badge m-2 badge-";
-        classes += (value === 0) ? "warning" : "primary"
+        classes += (this.state.value === 0) ? "warning" : "primary"
         return classes;
     }
 
-    function formatCount() {
-        let countValue = value;
-        return countValue === 0 ? "Zero" : countValue;
+    formatCount=()=>{
+        const value = this.state.value;
+        return value === 0 ? "Zero" : value;
     }
-
-    return (
-        <React.Fragment>
-            <span className={getBadgeClasses()}>{formatCount()}</span>
-            <button
-                onClick={() => {
-                    handleIncrement("some product/event")
-                }}
-                className="btn btn-secondary btn-sm">
-                Increment
-            </button>
-        </React.Fragment>
-    );
 }
+
+export default Counter;
+
+// export default function Counter(props) {
+//     ///TODO: fix props issue here
+//     const [value, setValue] = React.useState(props.counterValue);
+//
+//
+//     function handleIncrement(product) {
+//         console.log("Increment Clicked");
+//         let countValue = value;
+//         console.log(props);
+//         setValue(++countValue)
+//     }
+//
+//     function getBadgeClasses() {
+//         let classes = "badge m-2 badge-";
+//         classes += (value === 0) ? "warning" : "primary"
+//         return classes;
+//     }
+//
+//     function formatCount() {
+//         let countValue = value;
+//         return countValue === 0 ? "Zero" : countValue;
+//     }
+//
+//     return (
+//         <React.Fragment>
+//             <span className={getBadgeClasses()}>{formatCount()}</span>
+//             <button
+//                 onClick={() => {
+//                     handleIncrement("some product/event")
+//                 }}
+//                 className="btn btn-secondary btn-sm">
+//                 Increment
+//             </button>
+//         </React.Fragment>
+//     );
+// }
 
