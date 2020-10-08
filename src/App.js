7fd4f146125374b2829e60e8533b098aafa6003b
kapiljhajhria <jhajhria44@gmail.com>
2020-10-08 11:09:42 +0530

    added cart like counter in navbar

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
     return (
         <React.Fragment
         >
-            <NavBar/>
+            <NavBar totalCounters={counters.filter(c=>c.value>0).length}/>
             <main className="container">
                 <Counters
                     counters={counters}
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,10 +1,16 @@
-import React,{Component} from "react";
+import React, {Component} from "react";
 
-export default class NavBar extends Component{
+export default class NavBar extends Component {
     render() {
         return (
             <nav className="navbar navbar-light bg-light">
-                <a className="navbar-brand" href="#">Navbar</a>
+                <a className="navbar-brand" href="#">
+                    Navbar
+                    <span className="badge badge-pill badge-secondary m-2">
+                        {this.props.totalCounters}
+                    </span>
+                </a>
+
             </nav>
         );
     }
