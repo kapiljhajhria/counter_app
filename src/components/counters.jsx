61fe1eb9c6a9f8251e2e8c4e26f6ff6ff6772c5a
kapiljhajhria <jhajhria44@gmail.com>
2020-10-08 22:19:27 +0530

    updating phase

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,6 +2,11 @@ import React, {Component} from "react";
 import Counter from './counterComponent'
 
 class Counters extends Component {
+    componentDidUpdate(prevProps: Readonly<P>, prevState: Readonly<S>, snapshot: SS) {
+        console.log('prevProps',prevProps);
+        console.log('prevState',prevState);
+    }
+
     render() {
         const {onReset, counters, onDelete, onIncrement} = this.props;
         return (
